027d6cf24f76faaa42cf067bf50686541c7ada86
Wayne Zhu <way87767@gmail.com>
2020-08-23 23:22:06 -0400

    Updates

---
+++ precache-manifest.517bfa231738f64501530cb0e0b0ad10.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04550ba161e446302bfd7786f131d7ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3440c97b299291da5337",
+    "url": "/static/css/main.dbae107b.chunk.css"
+  },
+  {
+    "revision": "79eadcacf6f99c29d4b5",
+    "url": "/static/js/2.cfb19cf8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cfb19cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3440c97b299291da5337",
+    "url": "/static/js/main.67c9bf6a.chunk.js"
+  },
+  {
+    "revision": "062377468989df03ded3",
+    "url": "/static/js/runtime-main.c66b2ab1.js"
+  },
+  {
+    "revision": "498824600aa8e72f740f6d31ef79f759",
+    "url": "/static/media/2048.49882460.gif"
+  },
+  {
+    "revision": "b9920de0749addeb5a53462fb5bbe070",
+    "url": "/static/media/Inter-Light.b9920de0.woff"
+  },
+  {
+    "revision": "0555a46cda7fc3406a875d6bacf8fcff",
+    "url": "/static/media/Inter-LightItalic.0555a46c.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "1db6c55ca0f2a97fbce3b0b3903a7bb7",
+    "url": "/static/media/Inter-SemiBold.1db6c55c.woff"
+  },
+  {
+    "revision": "a65c27facf9255ba2d2fcbbccf5c9c60",
+    "url": "/static/media/aichess.a65c27fa.png"
+  },
+  {
+    "revision": "86fa854b582f41bc75b8bc187c35d298",
+    "url": "/static/media/fantasyBasketball(h).86fa854b.png"
+  },
+  {
+    "revision": "21671b6eb61a1fee44747dc866f55429",
+    "url": "/static/media/fantasyBasketball.21671b6e.png"
+  },
+  {
+    "revision": "30a0023500686dabb657dc272bf8d33f",
+    "url": "/static/media/fractal(h).30a00235.png"
+  },
+  {
+    "revision": "2333ca04a2d06e22f4f746c6834e7235",
+    "url": "/static/media/fractal.2333ca04.png"
+  },
+  {
+    "revision": "366212283fe1850d7196106c839646c9",
+    "url": "/static/media/galaga(h).36621228.gif"
+  },
+  {
+    "revision": "d7365b19e826a9f304fc685cc8a014ef",
+    "url": "/static/media/islandGenerator(h).d7365b19.gif"
+  },
+  {
+    "revision": "a89ff4e7b03a7358540e1ee61d62e4b6",
+    "url": "/static/media/islandGenerator.a89ff4e7.gif"
+  },
+  {
+    "revision": "ccc1708094ea14d23678206f368de066",
+    "url": "/static/media/pong(h).ccc17080.gif"
+  },
+  {
+    "revision": "af2a6fe3dc4cdfc8ed8b320bc7fbfca0",
+    "url": "/static/media/pong.af2a6fe3.gif"
+  },
+  {
+    "revision": "50bc3314723f8a6ecfdb2e69e82c23dd",
+    "url": "/static/media/takoyaki(h).50bc3314.gif"
+  },
+  {
+    "revision": "c40ee62afe859acd48d71fd67a67260e",
+    "url": "/static/media/talkToHand.c40ee62a.gif"
+  }
+]);
